5a745bf4f69dc7565e957948be5f681770bdfef7
Ben Howdle <benhowdle89@gmail.com>
2015-03-04 20:10:04 +0000

    img

--- controller.js
+++ controller.js
@@ -1,8 +1,5 @@
 var fs = require('fs');
-var gm = require('gm'),
-	imageMagick = gm.subClass({
-		imageMagick: true
-	});
+var gm = require('gm');
 
 module.exports = {
 	showRandomImage: function(req, res, next) {
@@ -14,14 +11,19 @@ module.exports = {
 			height = req.params.height || 500;
 
 		fs.readdir(dir(), function(err, files) {
+			var imageMagick = gm.subClass({
+				imageMagick: true
+			});
+			res.set('Content-Type', 'image/jpeg');
 			imageMagick(dir() + files[Math.floor(Math.random() * files.length - 1)])
 				.resize(width, height, '^')
 				.gravity('North')
 				.crop(width, height)
-				.stream('png', function(err, stdout) {
+				.stream('jpg', function(err, stdout) {
 					if (err) return next(err);
-					res.setHeader('Content-Type', 'image/png');
 					stdout.pipe(res);
+					stdout.on('error', next);
+					stdout.on('close', next);
 				});
 		});
 	}
